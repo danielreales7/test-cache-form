262449bb2f7a964b82d41781d9c73af13fbfe6a2
Daniel Reales <danielreales7@gmail.com>
2024-09-26 01:20:35 +0200

    first commit

---
+++ clear-cache.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Clear the cache and compile
+
+php artisan clear-compiled
+php artisan cache:clear
+php artisan config:clear
+php artisan debugbar:clear
+php artisan event:clear
+php artisan optimize:clear
+#php artisan queue:clear
+php artisan route:clear
+#composer dump-autoload
+
+echo Cache cleared!
+
+npm run dev
